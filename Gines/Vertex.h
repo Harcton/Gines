e08232ce541dbcd388135d0cd99982314d89cd6b
sathorael <sarielirc@gmail.com>
2015-11-06 13:12:27 +0200

    added comments for clarity

--- Gines/Vertex.h
+++ Gines/Vertex.h
@@ -4,14 +4,14 @@
 #include <GL/glew.h>
 #include <glm\glm.hpp>
 
-struct VertexPositionColorTexture
+struct VertexPositionColorTexture // For Sprites
 {
 	glm::vec2 position;
 	glm::vec4 color;
 	glm::vec2 uv;
 };
 
-struct VertexPositionColor
+struct VertexPositionColor // For Triangles
 {
 	glm::vec2 position;
 	glm::vec4 color;
